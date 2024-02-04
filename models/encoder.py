3cf591f7cff8ddff92103f2c1a346db0e3410729
Joel Lapin <joellapin@comcast.net>
2024-02-04 07:36:33 +0100

    Pairwise now working with new FourierFeatures function

--- models/encoder.py
+++ models/encoder.py
@@ -25,10 +25,16 @@ def init_encoder_weights(module):
         module.W1.bias = I.zeros_(module.W1.bias)
         module.W2.weight = I.normal_(module.W2.weight, 0.0, (1/3)*(module.indim*module.mult)**-0.5)
         module.W2.weight = I.xavier_uniform_(module.W2.weight)
+    #elif isinstance(module, Encoder):
+    #    module.MzSeq[0].weight = I.normal_(module.MzSeq[0].weight, 0.0, 0.03)
+    #    module.MzSeq[0].bias = I.zeros_(module.MzSeq[0].bias)
+    #    module.MzSeq[2].weight = I.normal_(module.MzSeq[2].weight, 0.0, 0.03)
+    #    module.MzSeq[2].bias = I.zeros_(module.MzSeq[2].bias)
     elif isinstance(module, nn.Linear):
         module.weight = I.xavier_uniform_(module.weight)
         if module.bias is not None:
             module.bias = I.zeros_(module.bias)
+    
 
 class Encoder(nn.Module):
     def __init__(self,
@@ -88,17 +94,21 @@ class Encoder(nn.Module):
         
         mdim = mz_units//4 if subdivide else mz_units
         self.mdim = mdim
-        self.MzSeq = nn.Identity()#nn.Sequential(nn.Linear(mdim, mdim), nn.SiLU())
+        self.MzSeq = nn.Identity()#Sequential(
+        #    nn.Linear(mz_units, 4*mz_units), 
+        #    nn.SiLU(),
+        #    nn.Linear(4*mz_units, mz_units)
+        #)
 
         # Pairwise mz
         if pairwise_bias:
             # - subidvide and expand based on mz_units, transform to pw_units
             mdimpw = self.pw_mzunits//4 if subdivide else self.pw_mzunits
             self.mdimpw = mdimpw
-            self.MzpwSeq = nn.Sequential(nn.Linear(mdimpw, mdimpw), nn.SiLU())
+            self.MzpwSeq = nn.Identity()#Sequential(nn.Linear(mdimpw, mdimpw), nn.SiLU())
             self.pwfirst = nn.Linear(self.pw_mzunits, self.pw_runits)
             self.alphapw = nn.Parameter(th.tensor(0.1), requires_grad=True)
-            self.pospw = pw.RelPos(sequence_length, self.pw_runits)
+            #self.pospw = pw.RelPos(sequence_length, self.pw_runits)
             # Evolve features
             multdict = {'in_dim': self.pw_runits, 'c': 128}
             attdict = {'in_dim': self.pw_runits, 'c': pw_attention_ch, 'h': pw_attention_h}
@@ -199,9 +209,9 @@ class Encoder(nn.Module):
             # expand based on mz_units
             if self.subdivide:
                 mzpw = mp.subdivide_float(dtsr)
-                mzpw_emb = mp.FourierFeatures(mzpw, self.mdimpw, 10000.)#.to(x.device)
+                mzpw_emb = mp.FourierFeatures(mzpw, 0.001, 10000, self.mdimpw)
             else:
-                mzpw_emb = mp.FourierFeatures(dtsr, self.mz_units, 10000.)#.to(x.device)
+                mzpw_emb = mp.FourierFeatures(dtsr, 2*np.pi, 10000, self.pw_mzunits)
             # transform based on pw_units
             mzpw_emb = self.MzpwSeq(mzpw_emb)
             mzpw_emb = mzpw_emb.reshape(x.shape[0], x.shape[1], x.shape[1], -1)
@@ -265,7 +275,7 @@ class Encoder(nn.Module):
         mabemb = mzab_dic['1d']
         pwemb = mzab_dic['2d']
         if self.pairwise_bias:
-            pwemb = self.pwfirst(pwemb) + self.alphapw * self.pospw() # REMOVE
+            pwemb = self.pwfirst(pwemb)# + self.alphapw * self.pospw()
             pwemb = self.PwSeq(pwemb)
         
         out = self.first(mabemb)# + self.alpha*self.pos[:x.shape[1]]
