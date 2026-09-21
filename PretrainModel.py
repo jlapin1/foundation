36f61ee45005f1327449bb56b88dba703732a140
Joel Lapin <joellapin@comcast.net>
2026-09-21 12:51:44 +0000

    Brought back dnconfig to dsconfig transfer settings. It was a mistake to get rid of them.

--- PretrainModel.py
+++ PretrainModel.py
@@ -69,6 +69,21 @@ header_dict = {task: header_dict['tasks'][task] for task in config['tasks']}
 # pass encoder_dict's running_units to header_dict as in_units
 header_dict['in_units'] = mconf['encoder_dict']['running_units']
 
+dnconfig['encoder_dict'] = {**mconf['encoder_dict'], 'empty': False}
+dnconfig['top_peaks'] = config['max_peaks']
+dnconfig['batch_size'] = config['batch_size']
+dnconfig['epochs'] = dsconfig['epochs']
+print(f"Denovo runs will last {dnconfig['epochs']} epochs")
+dnconfig['prev_wts'] = None
+dnconfig['pretrained_encoder_path'] = None # loading encoder inside denovo base
+dnconfig['loader']['val_name'] = dsconfig['loader']['val_species']
+dnconfig['freeze_encoder'] = dsconfig['freeze_encoder']
+dnconfig['save_weights'] = True if config['first_report']['only_dnv'] and config['svwts'] else False
+dnconfig['log_wandb'] = True if config['first_report']['only_dnv'] else False
+dnconfig['prev_wts'] = config['first_report']['loadpath'] if config['first_report']['only_dnv'] else None
+for key in dsconfig:
+    if 'lr_' in key: dnconfig[key] = dsconfig[key]
+
 # Denovo downstream evaluation
 if mconf['encoder_dict']['class_token']:
     config['nn_eval']['pooling'] = 'class_token'
